7e5a2a2530442c5d724717ca16cad610e7d302c1
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-03-27 04:36:38 +0700

    tugas php 1

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+// Variable
+$name = "Jimmy";
+$job = "Fullstack Web Developer";
+$email = "jimmywiraarbaa03@gmail.com";
+$telp = "021-12345";
+$wa = "+628123456754";
+$github = "jimmywiraarbaa";
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">Jimmy</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <div class="d-flex justify-content-center   align-items-center " style="margin-top: 120px;">
+        <div style="margin-right: 300px;">
+            <h2>Hy, Im <?php echo $name ?></h2>
+            <h4>I'm <?php echo $job ?></h4>
+            <button type="button" class="btn btn-primary mt-2 ">Here me</button>
+        </div>
+        <img class="w-25 ms-5   " src="profile-suit.png" alt="">
+    </div>
+
+    <div class="mt-5 text-center  ">
+        <h3 class="text-center ">Contact</h3>
+        <div class="mt-4 ">
+            <p>Email : <?php echo $email ?></p>
+            <p>Telp : <?php echo $telp ?></p>
+            <p>Whatsapp : <?php echo $wa ?></p>
+            <p>Github : <?php echo $github ?></p>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+
+</html>c:\xampp\htdocs\MSIB\tugas1\profile-suit.png
